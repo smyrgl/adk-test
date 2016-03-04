9025adb12c0bde7513cd3a86224e51d83328f0e0
John Tumminaro <jtumminaro@disqus.com>
2016-03-04 09:21:01 -0700

    Initial Commit

---
+++ adk_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  adk_test
+//
+//  Created by John Tumminaro on 3/4/16.
+//  Copyright © 2016 John Tumminaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
